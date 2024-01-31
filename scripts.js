d095051e1c232e6f8470f6f96394357c7c1a0165
Rakin Munim <rakin374@gmail.com>
2024-01-31 12:08:27 -0500

    new website content. added header and footer files for the website specifically

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+$(document).ready(function() {
+    // Load Header
+    $("#header-container").load("header.html");
+
+    // Load Footer
+    $("#footer-container").load("footer.html");
+});
